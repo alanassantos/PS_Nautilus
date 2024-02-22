3f8b27c22ba390e06eb1d0874d9aa20f388ce2fb
alanassantos <alanasilvasantos49@gmail.com>
2024-02-21 21:13:28 -0300

    Programa 1 e 2

---
+++ primo.py
@@ -0,0 +1,24 @@
+#2- Faça um programa que diga o maior divisor primo de um número dado como input
+
+def primo(numero):
+    divisor = 2
+    fatorado = []
+
+    while numero > 1:
+        if numero % divisor == 0:
+            fatorado.append(divisor)
+            numero = numero // divisor
+        else:
+            divisor += 1
+    
+    return max(fatorado) if fatorado else 1
+
+while True:
+    numero = int(input('Digite o número p/ saber seu maior divisor primo:  '))
+    if numero <= 0:
+        print('O número deve ser um inteiro positivo!')
+        continue
+
+    resultado = primo(numero)
+    print(f'P/ {numero} o maior divisor primo é {resultado}')
+    break
